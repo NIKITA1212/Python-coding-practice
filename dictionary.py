d4a682016d5b6d50eeaae4288c0a39896086e0d5
NIKITA TANK <NIKITA1212@users.noreply.github.com>
2017-05-08 11:59:36 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -35,3 +35,40 @@ Diffrent ways of creating dictionary
 
 dict(sape=4139, guido=4127, jack=4098)
 {'sape': 4139, 'jack': 4098, 'guido': 4127}
+
+-------
+
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
+
+output:
+
+gallahad the pure
+robin the brave
+
+----------
+
+When looping through a sequence, the position index and corresponding value can be retrieved at the same time using the enumerate() function.
+
+ for i, v in enumerate(['tic', 'tac', 'toe']):
+     print(i, v)
+
+Output: 
+ 
+0 tic
+1 tac
+2 toe
+
+-----
+
+To loop over two or more sequences at the same time, the entries can be paired with the zip() function.
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+     print('What is your {0}?  It is {1}.'.format(q, a))
+  
+What is your name?  It is lancelot.
+What is your quest?  It is the holy grail.
+What is your favorite color?  It is blue.
