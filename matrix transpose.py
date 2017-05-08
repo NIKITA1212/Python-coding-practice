2dfa527884434e24bb5a3e9fb063672ff4dd2366
NIKITA TANK <NIKITA1212@users.noreply.github.com>
2017-05-08 09:36:17 -0700

    Update matrix transpose.py

--- matrix transpose.py
+++ matrix transpose.py
@@ -1,6 +1,22 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
 p = [[row[i] for row in matrix] for i in range(len(matrix))]
 print(p)
 
 output: 
 [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+--------------------------------------------------
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+transposed = []
+  for i in range(len(matrix)):
+     transposed.append([row[i] for row in matrix])
+
+output: 
+[[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+-----------------------------------------------
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print (list(zip(*matrix)))
+
+output:
+  [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
