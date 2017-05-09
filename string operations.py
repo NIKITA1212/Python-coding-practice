5a9647a496d588328dacd6bce83d43e08b845d75
NIKITA TANK <NIKITA1212@users.noreply.github.com>
2017-05-08 17:18:41 -0700

    Create string operations.py

---
+++ string operations.py
@@ -0,0 +1,13 @@
+a = "this is nikita"
+print(a.capitalize())
+
+Output: This is nikita
+
+capitalize() will capitalizes the first letter of the string 
+---------------------------------------------
+a = "this is nikita"
+print(a.center(20,'_'))
+
+output: ___this is nikita___
+
+Returns a space-padded string with the original string centered to a total of width columns
